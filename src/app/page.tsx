4edb851ed570bdab9c82f77037f4b7da720cdddc
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2025-01-14 16:56:50 +0800

    gtm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Homepage from "@/components/features/Homepage";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Home() {
-  return <Homepage />;
+  return (
+    <>
+      <Homepage />
+      <GoogleAnalytics gaId="G-KE3B6ZGJCY" />
+    </>
+  );
 }
